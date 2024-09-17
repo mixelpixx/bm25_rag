c704a451b74826b4eb9388d44bc7ed772f78dd31
mixelpixx <chris_stockslager@comcast.net>
2024-09-17 00:29:41 -0400

    Updated start_project.bat to also launch the web page upon execution.

--- Backend/start_project.bat
+++ Backend/start_project.bat
@@ -9,7 +9,13 @@ REM Set the FLASK_APP environment variable
 set FLASK_APP=main.py
 
 REM Run the Flask server
-flask run
+start cmd /k "flask run"
+
+REM Wait for the server to start
+timeout /t 5 /nobreak >nul
+
+REM Open the default web browser to the local server
+start http://127.0.0.1:5000
 
 REM Pause to keep the command window open
 pause
